8ffcf50ea98388d4c33b15e6f9e103e66f6f8d67
Abneed <abneed.rodriguez@gmail.com>
2023-07-31 17:09:40 -0500

    Update user likes and fix issue of deserialize items

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -22,15 +22,13 @@ export const getItems = async (ids: string[]) => {
 
 	const results = await Promise.all(commands);
 
-	results.map((result, i) => {
+	return results.map((result, i) => {
 		if (Object.keys(result).length === 0) {
 			return null;
 		}
 
 		return deserialize(ids[i], result);
 	});
-
-	return results;
 };
 
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
